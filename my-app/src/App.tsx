1ac1acd38874c585fa7cae623793c4164fc34f14
Vinh Chu <vinhchugg@gmail.com>
2022-01-27 09:51:16 +0000

    basic signup form.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './components/Homepage';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-
+import SignUp from './components/Signup';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage/>} />     
+        <Route path="/" element={<Homepage/>} /> 
+        <Route path="/signup" element={<SignUp/>} />     
       </Routes>
     </BrowserRouter>
   );
