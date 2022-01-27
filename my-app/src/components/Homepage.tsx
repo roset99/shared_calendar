3ce541d6d4f89c0575239b1183771bf193b56ee3
Vinh Chu <vinhchugg@gmail.com>
2022-01-27 10:17:15 +0000

    basic homepage

--- my-app/src/components/Homepage.tsx
+++ my-app/src/components/Homepage.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Homepage() {
-  return <div>Hello, how are you</div>;
+  return (
+    <section id="hp">
+    <h1 id="hp-header">SHARED CALENDAR</h1>
+    <Link className="hp-signup" to="signup">Sign Up</Link>
+    <Link className="hp-login" to="/login">Login</Link>
+    </section>
+  );
 }
 
 export default Homepage;
