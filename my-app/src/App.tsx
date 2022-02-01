77cafb67b7747dc5c933460d318ce9f1c9864150
Rose <rosieeleanor@hotmail.com>
2022-02-01 16:05:17 +0000

    added family to sessionstorage and state in aop

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useState } from 'react';
 import './App.css';
 import Homepage from './components/Homepage';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
@@ -10,13 +10,35 @@ import Events from './components/Events';
 import DailyCalendar from './components/DailyCalendar';
 
 function App() {
+
+  const [currentFamily, setCurrentFamily] = useState(getSessionStorageOrDefault("currentFamily", null));
+
+  useEffect(() => {
+    sessionStorage.setItem("currentFamily", JSON.stringify(currentFamily));
+  }, [currentFamily]);
+
+  function getSessionStorageOrDefault(key: string, defaultValue: null) {
+    const stored = sessionStorage.getItem(key);
+
+    if (!stored) {
+      return defaultValue;
+    }
+    
+    return JSON.parse(stored);
+  }
+
+  const onLoginSetFamily = (family: any) : void => { 
+    setCurrentFamily(family);
+  }
+
+  
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Homepage/>} />
           <Route path="/month-calendar" element={<MonthlyCalendar />} />
-          <Route path="/signup" element={<SignUp/>} />  
-          <Route path="/login" element={<Login/>} />   
+          <Route path="/signup" element={<SignUp onLoginSetFamily={onLoginSetFamily}/>} />  
+          <Route path="/login" element={<Login onLoginSetFamily={onLoginSetFamily}/>} /> 
           <Route path="/events" element={<Events/>} /> 
           <Route path="/days" element={<DailyCalendar/>} /> 
       </Routes>
--- my-app/src/components/Login.tsx
+++ my-app/src/components/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-function Login():JSX.Element {
+function Login({onLoginSetFamily}):JSX.Element {
     return(
         <>
                 <form className="login">
