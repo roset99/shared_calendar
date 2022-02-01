b0f313efc099e7527640ae59506384bb79a452d5
jordan-s99 <jj1861@outlook.com>
2022-02-01 17:55:15 +0000

    css edits to login page

--- my-app/src/components/Login.tsx
+++ my-app/src/components/Login.tsx
@@ -5,20 +5,21 @@ function Login():JSX.Element {
     return(
         <>
                 <form className="login">
-                    <h1>Login</h1>
-                    <label htmlFor="name">Name</label>
-                    <input id="name" type="text" />
+                    <h1 className="login-title">Login</h1>
+                    <label htmlFor="name" className="login-input-label">Name</label>
+                    <input id="name" type="text" className="text-input" />
 
-                    <label htmlFor="psw">Password</label>
-                    <input id="psw" type="password" />
+                    <label htmlFor="psw" className="login-input-label">Password</label>
+                    <input id="psw" type="password" className="text-input"/>
 
+                    <div className="remember-div">
                     <label htmlFor="remember-me">Remember me</label>
                     <input type="checkbox" />
-
+                    </div>
                     <button type="submit" className="loginbtn">Login</button>
 
                     <p>Need an account?</p>
-                    <Link to="/signup">Sign up here</Link>
+                    <Link to="/signup" className="signup-link">Sign up here</Link>
                 </form>
         </>
     );
